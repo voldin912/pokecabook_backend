42def2d130302aec52683ff487f7483e8e5628a0
high-mountains <highmountain430@gmail.com>
2025-02-10 16:46:23 +0900

    [fix] getPlaceDetails-highmountain430-2025/02/10 16-46

--- controllers/placesController.js
+++ controllers/placesController.js
@@ -38,3 +38,17 @@ exports.getTotalPlaces = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+exports.getPlacesDetails = async (req, res) => {
+  console.log("🏢🏢👨‍💼 API FROM FRONTEND IS ARRIVED! 🏢👨‍💼");
+  try {
+    const { id } = req.params;
+    const query = `SELECT d.event_holding_id, d.deck_ID_var, d.rank_int, d.point_int FROM decks as d WHERE event_holding_id = ${id} ORDER BY point_int DESC, id ASC`;
+    const [events_result] = await db.query(query);
+    console.log('events_result==', events_result);
+    res.status(200).json(events_result);
+  } catch (err) {
+    console.error("Error fetching data:", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- routes/places.js
+++ routes/places.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getPlaces, getTotalPlaces } = require("../controllers/placesController");
+const { getPlaces, getTotalPlaces, getPlacesDetails } = require("../controllers/placesController");
 
 const router = express.Router();
 
 router.post("/place-card", getPlaces);
 router.get("/place-card/total", getTotalPlaces);
+router.get("/place-card/:id", getPlacesDetails);
+
 
 module.exports = router;
